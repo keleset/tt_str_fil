0a91014e5b8de27e867425577787c2ea45fe0f9a
keleset <keleset@gmail.com>
2019-04-13 01:41:30 +0300

    New algorithm + Utf-8 support commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,72 @@
+use std::collections::VecDeque;
+
+struct StreakList { //Represents queue of characters
+    list: VecDeque<(usize, char)>,
+    max_size: usize, //Max size of the queue
+}
+
+impl StreakList { //Constructor
+    fn new(max_size: usize) -> StreakList {
+        StreakList {
+            list: VecDeque::new(),
+            max_size
+        }
+    }
+
+    fn push(&mut self, new_elem: (usize, char)) -> Option<(usize, char)>{ //Method for pushing values to the queue
+        'list_iter: for (i, &elem) in self.list.iter().enumerate() { //Check if the queue already contains this char
+            if elem.1 == new_elem.1 { //If it does, we need to move it to the beginning, so remove it from the old position:
+                self.list.remove(i);
+                break 'list_iter;
+            }
+        }
+
+        self.list.push_back(new_elem); //Pushing tuple (position_in_text, character) to the queue
+        
+        if self.list.len() > self.max_size { //If queue overfilled, pop and return the oldest character
+            return self.list.pop_front();
+        }
+        None //Returning None if queue is still in limits
+    }
+}
+
 fn string_filter_base(strg: &String, base_len: usize) -> &str {
     
     //Checking input string:
     if strg.len() <= base_len {
         return &strg[..];
     }
-    
-    //Dividing strg to byte array
-    let strg_bytes = strg.as_bytes();
 
-    //Storing max length and starting point for max length streak:
+    //Storing length and starting point for max and current streak:
     let mut max_len = base_len;
     let mut max_start: usize = 0;
+    let mut curr_len: usize = 0;
+    let mut curr_start: usize = 0;
 
-    //Iterating through bytes of the input string:
-    for curr_start in 0..strg_bytes.len() { 
-        
-        let mut curr_bytes = Vec::with_capacity(base_len); //Vector used as storage for characters of the current streak
-        let mut curr_len: usize = 0; //Length of the current streak
-        
-        //Checking each byte as beginning of the streak:
-        'streak_iter: for i in curr_start..strg_bytes.len() {
+    let mut streak_bytes = StreakList::new(base_len);
 
-            if curr_bytes.contains(&strg_bytes[i]) { //Checking if storage already contains current character
-                curr_len += 1; //Yes, increase current streak length
-            } else if curr_bytes.len() < base_len { //No, but storage is not full yet
-                curr_bytes.push(strg_bytes[i]); //Adding character to storage
-                curr_len += 1; //Increasing streak
-            } else { //New character, but storage is full:
-                if curr_len > max_len { //Store the result, if worth storing
+    //Iterating through characters of the input string:
+    'strg_iter: for (i, curr_char) in strg.char_indices() {
+
+        match streak_bytes.push((i, curr_char)) {
+            //Something returned, that means something was popped
+            //from the queue as result of pushing through max_size limit,
+            //that means that streak is broken and now we have to
+            //record streak (if worth recording) and
+            //use the data about the position of the oldest
+            //(popped) character from the queue to determine the start
+            //position for new streak:
+            Some((drop_elem_pos, _drop_elem)) => {
+                if curr_len > max_len {
                     max_len = curr_len;
                     max_start = curr_start;
                 }
-                break 'streak_iter; //Breaking streak
+                curr_start = drop_elem_pos + curr_char.len_utf8();
+                curr_len = i - drop_elem_pos;
+            }
+            //None returned means that streak continues:
+            None => {
+                curr_len += curr_char.len_utf8();
             }
         }
     }
--- src/tests.rs
+++ src/tests.rs
@@ -1,41 +1,51 @@
 #[test]
-fn test_01() {
+fn test_lat_01() {
     let test_string = String::from("teststststring");
     assert_eq!(super::string_filter(&test_string), "stststst");
 }
 
 #[test]
-fn test_02() { 
+fn test_lat_02() { 
     let test_string = String::from("tesssssstststring");
     assert_eq!(super::string_filter(&test_string), "sssssststst");
 }
 
 #[test]
-fn test_03() { 
+fn test_short_03() { 
     let test_string = String::from("te");
     assert_eq!(super::string_filter(&test_string), "te");
 }
 
 #[test]
-fn test_04() { 
+fn test_short_04() { 
     let test_string = String::from("t");
     assert_eq!(super::string_filter(&test_string), "t");
 }
 
 #[test]
-fn test_05() { 
+fn test_empty_05() { 
     let test_string = String::from("");
     assert_eq!(super::string_filter(&test_string), "");
 }
 
 #[test]
-fn test_06() { 
+fn test_upper_06() { 
     let test_string = String::from("tessssssTststring");
     assert_eq!(super::string_filter(&test_string), "ssssssTs");
 }
 
 #[test]
-fn test_07() { 
+fn test_special_07() { 
     let test_string = String::from("@$@##%%%%$$%#^#@$@");
     assert_eq!(super::string_filter(&test_string), "%%%%$$%");
+}
+#[test]
+fn test_cyr_08() { 
+    let test_string = String::from("тессстстстринг");
+    assert_eq!(super::string_filter(&test_string), "ссстстст");
+}
+#[test]
+fn test_mix_09() { 
+    let test_string = String::from("те虎с虎с虎с虎тстстринг");
+    assert_eq!(super::string_filter(&test_string), "虎с虎с虎с虎");
 }
